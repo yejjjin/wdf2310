eeb31b354e294f4314bd00d996e03f6374264fdb
yejjjin <ggreat7712@gmail.com>
2024-01-23 12:12:16 +0900

    js basic study : 매개변수 패턴 / 화살표 함수

--- 07-js/js/js 함수.js
+++ 07-js/js/js 함수.js
@@ -81,10 +81,78 @@ console.log(getEmail(users[3]));
 const animals = ["dog", "cat", "fish"];
 
 // 함수 선언 (배열의 두번째 값 출력 함수)
-function getSecond(array) {
-  const [a, b, c] = array;
-  return `난${a},${b},${c}키우고 있어`;
+function getSecond([, b = "두번째 요소가 없습니다."]) {
+  // const [a, b, c] = array;
+  return `난 ${b}를 키우고 있어`;
 }
 
 // 함수 호출
 console.log(getSecond(animals));
+
+// 나머지 매개변수 (...rest) : 매개변수를 배열로 전달받는다
+// function 함수이름 (...매개변수) {}
+
+function sum(a, b, ...rest) {
+  console.log(a, b, rest);
+}
+
+sum(1, 2);
+sum(1, 2, 3, 4, 5, 6, 7);
+
+// 배열 데이터의 숫자를 더하는 로직 함수
+function sumAll(...rest) {
+  console.log(rest);
+
+  // return rest.reduce(fn, 초기값);
+  return rest.reduce(function (account, current) {
+    return account + current;
+  }, 0);
+}
+
+console.log(sumAll(1, 2));
+console.log(sumAll(1, 2, 3, 4, 5, 6, 7));
+
+// 화살표 함수 (Arrow Function)
+// 기본 - const 함수이름 = function () {}; : 함수의 표현식
+
+// () => {}; : 매개변수가 없을 경우
+// x =>; : 매개변수가 하나일 경우
+// (x,y) => {}; : 매개변수가 두 개 이상일 경우 소괄호로 감싸준다
+
+const hello1 = (name) => {
+  console.log(`나는 ${name}입니다.`);
+};
+
+hello("김코딩");
+
+const sum2 = (a, b) => {
+  return a + b;
+};
+
+console.log(sum2(1, 2));
+
+// 화살표 함수의 다양한 예시
+// 1. 매개변수가 없는 경우
+const a = () => {};
+// 2. 매개변수가 하나인 경우 (괄호 생략 가능)
+const b = (x) => {};
+// 3. 매개변수가 여러개인 경우 (괄호 생략 불가)
+const c = (x, y) => {};
+// 4. 함수의 return이 있는 경우 (중괄호 포함)
+// return으로 시작하는 경우 중괄호, return 생략 가능
+const d = (x, y) => {
+  return x + y;
+};
+// 5. return으로 시작하지 않는 경우 중괄호 생략불가
+const e = (x, y) => {
+  console.log(x);
+  return y;
+};
+// 6. 객체를 반환return하는 경우 (객체를 소괄호로 감싸준다.)
+const f = (x) => {
+  return { a: x };
+};
+// 7. 배열을 반환하는 경우 (중괄호 생락가능)
+const g = () => {
+  return [1, 2];
+};
