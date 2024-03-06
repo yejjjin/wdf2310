6d351c0a9b28effe2e43e6e6787e5806625b21e2
yejjjin <ggreat7712@gmail.com>
2024-03-06 11:54:39 +0900

    함수

--- 07-js/js/정리.js
+++ 07-js/js/정리.js
@@ -163,3 +163,97 @@ for (let lover of lovers) {
 
 console.log(`${lovers[0].name}의 나이는 ${lovers[0].age}살 입니다.`);
 console.log(`${lovers[1].name}의 나이는 ${lovers[1].age}살 입니다.`);
+
+// 함수
+// 선언 후 호출 해야 하는데 일반함수는 호이스팅이 일어나기 때문에 호출 후 선언도 가능
+hello();
+hello2();
+
+function hello() {
+  console.log("안녕하세요");
+}
+// 익명함수 (표현식)
+const hello2 = function () {
+  console.log("안녕하세요 표현식입니다.");
+};
+
+// 매개변수
+function sum(a, b = 5) {
+  return a + b;
+}
+console.log(sum(1, 2));
+console.log(sum(1));
+
+// 객체 구조분해 할당
+const user = {
+  name: "dd",
+  age: 12,
+  email: "",
+};
+// 일반
+function getName(props) {
+  return `${props.name}은 ${props.age}살 입니다.`;
+}
+// 객체 구조분해 할당
+function getName(props) {
+  const { name, age } = props;
+  return `${name}은 ${age}살 입니다.`;
+}
+function getName({ name, age, email = "이메일이 없습니다." }) {
+  // const { name, age } = props;
+  return `${name}은 ${age}살 입니다. ${email}`;
+}
+console.log(getName(user));
+
+// 배열의 구조분해 할당
+const table = [row1, row2, row3];
+function get2([a, b, c]) {
+  const [a, b = "기본값", c] = props;
+
+  return b;
+}
+console.log(get2(table));
+
+function sum4(...rest) {
+  return rest;
+}
+
+console.log(sum4("1,2,3,4,5,6,7"));
+
+// 화살표 함수
+const arrow = () => {
+  console.log(arrow());
+};
+
+arrow();
+
+// 매개변수 없는 경우
+const aa = () => {};
+// 매개변수 하나인 경우
+const bb = (x) => {};
+// 매개변수 여러개인 경우
+const cc = (x, y) => {};
+// 함수의 리턴이 있는 경우
+const dd = (x, y) => {
+  return x + y;
+};
+// 축약된 형태 (리턴 생략)
+const ee = (x, y) => x + y;
+// 리턴 키워드로 시작하지 않는 경우
+// 중괄호를 포함한 리턴을 생략할 수 없다.
+const ff = (x, y) => {
+  console.log(x + y);
+  return x + y;
+};
+// 객체데이터를 반환하는 경우
+const gg = () => {
+  return { a: 1, b: 2 };
+};
+// 축약형 객체의 중괄호와 화살표 함수의 중괄호를 구분하기 위해 소괄호 사용
+const hh = () => ({ a: 1, b: 2 });
+// 배열 데이터를 반환하는 경우
+const ii = () => {
+  return [1, 2, 3];
+};
+// 배열 데이터 축약형
+const jj = () => [1, 2, 3];
