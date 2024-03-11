66d436dda104b349bac95f27b91685688d84652e
yejjjin <ggreat7712@gmail.com>
2024-03-11 15:04:32 +0900

    표준 내장 객체 정의

---
+++ 07-js/js/표준내장객체.js
@@ -0,0 +1,12 @@
+console.log(new Date());
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth());
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+
+console.log(new Date().getHours());
+console.log(new Date().getMinutes());
+console.log(new Date().getSeconds());
+console.log(new Date().getMilliseconds());
+
+console.log(new Date().getTime());
